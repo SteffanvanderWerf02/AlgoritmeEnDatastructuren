7b75da4a790aea78898b33ecb6ccc56e253947d2
SteffanvanderWerf02 <steffan.van.der.werf@student.nhlstenden.com>
2023-02-07 10:00:13 +0100

    bad big O

--- AlgoritmeEnDatastructurenOpdrachten/src/main/java/com/nhlstenden/bigoexamples/bad_bigO/Example.java
+++ AlgoritmeEnDatastructurenOpdrachten/src/main/java/com/nhlstenden/bigoexamples/bad_bigO/Example.java
@@ -1,4 +1,10 @@
 package com.nhlstenden.bigoexamples.bad_bigO;
 
 public class Example {
+    public int factorial(int n) {
+        for (int i = 1; i <= factorial(n); i++) {
+            System.out.println("Hey - I'm busy looking at: " + i);
+        }
+    }
+
 }
